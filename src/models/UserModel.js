9b8e8a252ef1936313fca234fd4b14ecc924c040
Tim <tim.maastricht@gmail.com>
2026-03-16 20:06:39 +1000

    fix: refactor user model

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -2,103 +2,121 @@ const mongoose = require("mongoose");
 const crypto = require("node:crypto");
 
 const UserSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      minLength: [8, "Username too short"],
-    },
-    password: {
-      type: String,
-      required: true,
-      minLength: [8, "Password too short"],
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-      required: false,
-    },
-    usersWeFollow: {
-      type: [
-        {
-          type: mongoose.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-      default: [],
-    },
-    postsWeReactedTo: {
-      type: [
-        {
-          type: mongoose.Types.ObjectId,
-          ref: "Post",
-        },
-      ],
-      default: [],
-    },
-    salt: {
-      type: String,
-      required: true,
-      default: generateSalt,
-    },
-  },
-  {
-    timestamps: true,
-  },
+	{
+		// username, email, etc
+		username: {
+			unique: true,
+			required: true,
+			type: String,
+			// at least 3 characters long!!
+			minLength: [3, "Username too short!"] 
+		},
+		password: {
+			required: true,
+			type: String,
+			// at least 8 characters long!!
+			minLength: [8, "Password too short!"] 
+		},
+		email: {
+			unique: true,
+			type: String,
+			required: true,
+			// should be in an email format
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+			required: false
+		},
+		usersWeFollow: {
+			type: [{
+				type: mongoose.Types.ObjectId,
+				ref: "User"
+			}],
+			default: []
+		},
+		postsWeReactedTo: {
+			type: [{
+				type: mongoose.Types.ObjectId,
+				ref: "Post"
+			}],
+			default: []
+		},
+		salt : {
+			type: String,
+			required: true,
+			default: generateSalt
+		}
+	},
+	{
+		timestamps: true,
+	}
 );
 
-function generateSalt() {
-  return crypto.randomBytes(64).toString("hex");
+function generateSalt(){
+	return crypto.randomBytes(64).toString("hex");
 }
 
-// 1. add a middleware/hook to hash the password when the password is updated
+
+// 1. Add a middleware/hook to hash the password when the password is updated
 UserSchema.pre("save", function (next) {
-  // 1. check if the sale is available for this user
-  //  if there is no salt on the user
-  if (!this.salt) {
-    this.salt = generateSalt();
-  }
 
-  if (!this.isModified("password")) return;
+	// 1. Check if the salt is available for this user
+	// 		if not, create a salt!
+	if (!this.salt){
+		this.salt = generateSalt();
+	}
 
-  this.password = crypto
-    .scryptSync(this.password, this.salt, 64)
-    .toString("hex");
+	// Only proceed if the user password was modified!
+	if (!this.isModified("password")) return;
 
-  // this might only be for post hooks now
-  // next();
+	// if we reach this point in the middleware hook, then the password
+	// needs to be rehashed!!!
+
+	this.password = crypto.scryptSync(this.password,  this.salt, 64).toString("hex");
+
+	// leaving out next() because it might only be for post hooks nowadays!
+	// next();
+
 });
 
-// 2.
-UserSchema.methods.comparePassword = function (incomingPasswordToCheck) {
-  // hash incoming password and check if hashed version matches
 
-  if (!this.salt) {
-    this.salt = generateSalt();
-  }
+// 2. Compare passwords!
+UserSchema.methods.comparePassword = function(incomingPasswordToCheck) {
 
-  let hashedAndSaltedIncomingPassword = crypto
-    .scryptSync(incomingPasswordToCheck, this.salt, 64)
-    .toString("hex");
+	if (!this.salt){
+		this.salt = generateSalt();
+	}
 
-  if (hashedAndSaltedIncomingPassword == this.password) {
-    // passwords match
-    return true;
-  } else {
-    return false;
-  }
-};
-// foundUser.comparePassword(request.body.loginPassword)
+
+	// Can't compare as-is, 
+	// because this.password is hashed and incomingPassword is not hashed
+	// if (incomingPasswordToCheck == this.password) 
+	// We need to hash incomingPasswordToCheck and see if the hashed version matches!
+
+	let hashedAndSaltedIncomingPassword = crypto.scryptSync(incomingPasswordToCheck, this.salt, 64).toString("hex");
+
+	if (hashedAndSaltedIncomingPassword == this.password){
+		// passwords match!
+		return true;
+	} else {
+		// passwords do NOT match!
+		return false;
+	}
+}
+// foundUser.comparePassword(request.body.loginpassword)
+
+
+
+
 
 const UserModel = mongoose.model("User", UserSchema);
 
+
 module.exports = {
-  UserSchema,
-  UserModel,
-};
+	UserSchema,
+	UserModel
+}
+
+
+
